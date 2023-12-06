52f748cb4e8b88ff425cfa5fe1a5f8afba197462
GP <gregpetropoulos@yahoo.com>
2023-12-05 21:03:56 -0500

    Add business logic to newsletter

--- frontend/app/_components/Blog/SubscribeToNewsletter.jsx
+++ frontend/app/_components/Blog/SubscribeToNewsletter.jsx
@@ -1,25 +1,53 @@
-// 'use-client';
+'use client';
+import { useState } from 'react';
 
 const SubscribeToNewsletter = () => {
+  const [email, setEmail] = useState('');
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (email.length) {
+      setIsSignedUp(true);
+      setEmail('')
+    }
+
+    // send email to strapi
+    // user feedback
+  };
   return (
     <section className='newsletter'>
-      <div className='newsletter__info'>
-        <h4>subscribe to our newsletter</h4>
-        <p className='copy'>
-          Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
-          Newsletter Today to always stay in touch
-        </p>
-      </div>
-      <form className='newsletter__form'>
-        <input
-          type='text'
-          className='newsletter__email input'
-          placeholder='Enter your E-mail address'
-        />
-        <button className='newsletter__subscribe btn btn--medium btn--turquoise'>
-          SUBSCRIBE
-        </button>
-      </form>
+      {isSignedUp ? (
+        <h4 className='newsletter__thanks'>
+          Thank you for signing up for our newsletter
+        </h4>
+      ) : (
+        <>
+          <div className='newsletter__info'>
+            <h4>subscribe to our newsletter</h4>
+            <p className='copy'>
+              Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
+              Newsletter Today to always stay in touch
+            </p>
+          </div>
+          <form className='newsletter__form' onSubmit={onSubmit}>
+            <input
+              type='text'
+              value={email}
+              onChange={onChange}
+              className='newsletter__email input'
+              placeholder='Enter your E-mail address'
+            />
+            <button
+              type='submit'
+              className='newsletter__subscribe btn btn--medium btn--turquoise'>
+              SUBSCRIBE
+            </button>
+          </form>
+        </>
+      )}
     </section>
   );
 };
